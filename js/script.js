2f20439f67c9f6b9e3f09b75c55aadb586f13226
DiegoValdivieso22 <159164145+DiegoValdivieso22@users.noreply.github.com>
2024-04-11 00:53:18 +0000

    edit

--- js/script.js
+++ js/script.js
@@ -1,5 +1,14 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
-//
-// Created by: Mr. Coxall
-// Created on: Sep 2020
-// This file contains the JS functions for index.html
+function doMathClicked() {
+  // this function does basic math: Area of a Trapezoid
+  let  radius = parseInt(document.getElementById("radius").value);
+  
+  let answer = (4/3) * Math.PI * Math.pow(radius, 3);
+
+  // Make sure not negative
+  if (answer <= 0) {
+    answer = 0;
+    document.getElementById("Radius").innerHTML = "Invalid input. Ensure value is a positive number.";
+  } else {
+    document.getElementById("result").innerHTML = "Volume of a sphere! = " + answer.toFixed(2);
+  }
+}
